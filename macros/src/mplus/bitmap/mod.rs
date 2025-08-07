3618043c35e0216d0b10f5df4ecf8721e0fd2847
iddey <iddengo@protonmail.com>
2025-08-07 08:45:00 +0200

    refactor(macros): implement newtype pattern

--- macros/src/mplus/bitmap/mod.rs
+++ macros/src/mplus/bitmap/mod.rs
@@ -7,7 +7,7 @@ mod units;
 
 pub mod from_outline;
 
-pub use dict::CharDictionary;
+pub use dict::{CharDictionary, CharDictionaryKey};
 pub use font::BitmapFont;
 pub use glyph::{Glyph, GlyphList};
 pub use image::{Image, ImageList};
